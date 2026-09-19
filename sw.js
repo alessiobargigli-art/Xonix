b34be1b0443bb2f36732970cdb180d0cf62dad48
alessiobargigli-art <alessio.bargigli@gmail.com>
2026-09-20 01:54:03 +0200

    fix: size mobile playfield directly from viewport v0.3.28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='0.3.27';
+const VERSION='0.3.28';
 const CACHE='xonix-'+VERSION;
 const CORE=['./','./index.html','./style.css?v='+VERSION,'./game.js?v='+VERSION,'./manifest.webmanifest','./icon.svg'];
 
