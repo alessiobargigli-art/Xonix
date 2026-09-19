2978e75a7514e02404549e2a5c667688e396f7a9
alessiobargigli-art <alessio.bargigli@gmail.com>
2026-09-20 01:41:29 +0200

    fix: letterbox mobile playfield without cropping v0.3.20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='0.3.19';
+const VERSION='0.3.20';
 const CACHE='xonix-'+VERSION;
 const CORE=['./','./index.html','./style.css?v='+VERSION,'./game.js?v='+VERSION,'./manifest.webmanifest','./icon.svg'];
 
