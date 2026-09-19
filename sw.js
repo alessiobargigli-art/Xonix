1d939c10d6bda4046e19c4369228dd8d15abd8a3
alessiobargigli-art <alessio.bargigli@gmail.com>
2026-09-20 01:36:33 +0200

    fix: keep mobile UI inside viewport without scrolling v0.3.17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='0.3.16';
+const VERSION='0.3.17';
 const CACHE='xonix-'+VERSION;
 const CORE=['./','./index.html','./style.css?v='+VERSION,'./game.js?v='+VERSION,'./manifest.webmanifest','./icon.svg'];
 
